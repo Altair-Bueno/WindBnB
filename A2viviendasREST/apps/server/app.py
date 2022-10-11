f709dbd0573601eb126ac930573ea6cfb2b14049
José Luis Bueno <74137433+jxtaaa@users.noreply.github.com>
2022-10-11 20:19:55 +0200

    Empezando micro

---
+++ A2viviendasREST/apps/server/app.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# Los tags son identificadores para agrupar rutas. Las rutas con
+# mismas etiquetas se agrupan en una sección de la documentación
+# de la API
+@app.get("/", tags=["Root"])
+async def read_root():
+    return{"message": "Welcome."}
