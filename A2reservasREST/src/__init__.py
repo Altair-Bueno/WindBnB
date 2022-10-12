f40d262c1368bcb8c53808d0b9e023b420697eda
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-10-12 12:03:10 +0200

    docs(reservas): Updated openapi scheme

--- A2reservasREST/src/__init__.py
+++ A2reservasREST/src/__init__.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.openapi.utils import get_openapi
 from fastapi.responses import JSONResponse
 
 from .beans import get_settings
 from .model import Message
 from .routes import BaseRouter
+
+__name__ = "A2reservasREST"
+__version__ = "0.1.0"
+__docs__ = "Gestión de reservas. Ingeniería web"
 
 app = FastAPI()
 app.include_router(BaseRouter)
@@ -15,6 +20,29 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title=__name__,
+        version=__version__,
+        description=__docs__,
+        routes=app.routes,
+    )
+
+    # Include servers section
+    openapi_schema["servers"] = [
+        {"url": "/", "description": "Default"},
+        {"url": "http://localhost:8080", "description": "Docker compose"},
+    ]
+
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
 
 
 @app.exception_handler(KeyError)
