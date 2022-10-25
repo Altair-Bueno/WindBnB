469f6e589fcbf1c777bd4140628191eb346e1fba
José Luis Bueno <jlbp@uma.es>
2022-10-25 20:39:57 +0200

    Microservicio bueno hecho por Tapia y Bueno.

---
+++ A2viviendasREST_nuevo/app/dependencies.py
@@ -0,0 +1,39 @@
+from functools import lru_cache
+from fastapi import Depends
+from motor.motor_asyncio import AsyncIOMotorClient
+
+from .settings import Settings
+
+
+
+@lru_cache
+def get_settings() -> Settings:
+    return Settings()
+
+
+@lru_cache
+def get_mongo_client(config: Settings = Depends(get_settings)):
+    # set a 5-second connection timeout
+    return AsyncIOMotorClient(config.mongo.url, serverSelectionTimeoutMS=5000)
+
+
+@lru_cache
+def get_mongo_database(
+    client=Depends(get_mongo_client), settings: Settings = Depends(get_settings)
+):
+    return client[settings.mongo.database]
+
+
+@lru_cache
+def get_windbnb_collection(
+    database=Depends(get_mongo_database), settings: Settings = Depends(get_settings)
+):
+    return database[settings.mongo.collection]
+
+
+"""
+@lru_cache
+def get_booking_service(collection=Depends(get_windbnb_collection)) -> BookingService:
+    return BookingService(collection)
+"""
+
---
+++ A2viviendasREST_nuevo/app/settings.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, BaseSettings
+
+
+class MongoConfig(BaseModel):
+    url: str
+    collection: str
+    database: str
+
+    class Config:
+        frozen = True
+
+
+class Settings(BaseSettings):
+    mongo: MongoConfig
+    # aws: AWSConfig
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+        env_nested_delimiter = "_"
+        frozen = True
