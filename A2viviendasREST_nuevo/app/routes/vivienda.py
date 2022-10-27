ad8c947605062ff697d680876b310ecbc384be5e
José Luis Bueno <jlbp@uma.es>
2022-10-27 12:28:31 +0200

    Funcionando docker.

--- A2viviendasREST_nuevo/app/routes/vivienda.py
+++ A2viviendasREST_nuevo/app/routes/vivienda.py
@@ -2,6 +2,7 @@ from bson import ObjectId
 from fastapi import APIRouter, Depends
 from fastapi.openapi.models import Response
 from fastapi.responses import Response
+from pymongo import ReturnDocument
 
 from app.models.vivienda import Vivienda
 from ..dependencies import get_windbnb_collection
@@ -13,7 +14,12 @@ vivienda = APIRouter()
 async def create_house(vivienda: Vivienda, collection=Depends(get_windbnb_collection)):
     new_house = dict(vivienda)
     result = await collection.insert_one(new_house)
-    print(f'{result.inserted_id=}')
+    return await collection.find_one({"_id": ObjectId(result.inserted_id)}, {"_id": 1,
+                                                                             "title": 1,
+                                                                             "description": 1,
+                                                                             "user_id": 1,
+                                                                             "location": 1,
+                                                                             "state": 1})
 
 
 @vivienda.get('/viviendas/{idCasa}', response_model=Vivienda)
@@ -29,10 +35,11 @@ async def find_house(idCasa: str, collection=Depends(get_windbnb_collection)):
 
 
 @vivienda.put('/viviendas/{idCasa}', response_model=Vivienda)
-async def update_house(idCasa: str, vivienda:Vivienda, collection=Depends(get_windbnb_collection)):
-    result = await collection.find_one_and_update({"_id": ObjectId(idCasa)}, {"$set": dict(vivienda)})
+async def update_house(idCasa: str, vivienda: Vivienda, collection=Depends(get_windbnb_collection)):
+    result = await collection.find_one_and_update({"_id": ObjectId(idCasa)}, {"$set": dict(vivienda)},
+                                                  return_document=ReturnDocument.AFTER)
     if result:
-        return Response(status_code=200)
+        return result
     else:
         return Response(status_code=404)
 
