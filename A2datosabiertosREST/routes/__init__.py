c5e5d7d201096187e2d0b3ae5b04ba4eb52d8ad9
carmengo01 <carmela050701@gmail.com>
2022-10-31 19:43:22 +0100

    new: average_stay gov data start

--- A2datosabiertosREST/routes/__init__.py
+++ A2datosabiertosREST/routes/__init__.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
-from routes import gas_stations
+from routes import gas_stations, average_stay
 
 router = APIRouter()
 router.include_router(gas_stations.router, tags=["Gas Stations"])
+router.include_router(average_stay.router, tags=["Average Stay"])
---
+++ A2datosabiertosREST/routes/average_stay.py
@@ -0,0 +1,21 @@
+from typing import Optional
+
+from fastapi import APIRouter, Depends
+
+from models.average_stay import AverageStayFilter
+from services.average_stay import AverageStayService
+
+router = APIRouter()
+
+
+@router.get("/average-stay")
+async def get_average_stay(
+        provincia: str,
+        mes: Optional[str] = None,
+        service: AverageStayService = Depends(AverageStayService),
+):
+    average_stay_filter = AverageStayFilter(
+        provincia=provincia,
+        mes=mes
+    )
+    return await service.find_average_stay(average_stay_filter)
