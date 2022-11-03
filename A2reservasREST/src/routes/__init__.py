617e34bda1fe4bb1376f2bfeb95011173cc2bcbb
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-11-03 14:13:48 +0100

    docs(reservas): Added tags to reservas

--- A2reservasREST/src/routes/__init__.py
+++ A2reservasREST/src/routes/__init__.py
@@ -5,10 +5,10 @@ from ..model import Message
 
 BaseRouter = APIRouter()
 
-BaseRouter.include_router(booking_router, prefix="/booking")
+BaseRouter.include_router(booking_router, prefix="/booking", tags=["Booking"])
 
 
-@BaseRouter.get("/ping", response_model=Message)
+@BaseRouter.get("/ping", response_model=Message, tags=["Booking"])
 async def ping():
     """Responds with an "Available" message, if the service is available"""
     return Message(message="Available")
