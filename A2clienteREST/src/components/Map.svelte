a0bd19f33c6ef0ab0b3b9d67076d9eefdf56c88b
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-12-20 17:00:31 +0100

    ref(cliente): Removed example pages

--- A2clienteREST/src/components/Map.svelte
+++ A2clienteREST/src/components/Map.svelte
@@ -1,36 +1,49 @@
 <script lang="ts">
-    import type { Map } from "leaflet";
-    import type { EESSPrecio } from "../api/A2datosabiertosREST";
-    import type { Coordinates } from "../data/types/map";
+  import "leaflet/dist/leaflet.css";
+  import type { Map } from "leaflet";
+  import type { EESSPrecio } from "../api/A2datosabiertosREST";
+  import type { Coordinates } from "../data/types/map";
 
-    function setMap(mapElement: HTMLElement) {
-        (async () => {
-            const L = await import("leaflet")
-            const { addHouseToMap, addGasStationsToMap } = await import("../utils/map-utils");
+  function setMap(mapElement: HTMLElement) {
+    (async () => {
+      const L = await import("leaflet");
+      const { addHouseToMap, addGasStationsToMap } = await import(
+        "../utils/map-utils"
+      );
 
-            var map: Map = L.map(mapElement).setView([coordinates.latitude, coordinates.longitude], 13);
-            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                maxZoom: 19,
-                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-            }).addTo(map);
+      var map: Map = L.map(mapElement).setView(
+        [coordinates.latitude, coordinates.longitude],
+        13
+      );
+      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        maxZoom: 19,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      }).addTo(map);
 
-            addHouseToMap(L, coordinates, map);
-            addGasStationsToMap(L, gasStations, map);
-        })()
-    }
+      addHouseToMap(L, coordinates, map);
+      addGasStationsToMap(L, gasStations, map);
+    })();
+  }
 
-    /**
-     * param: coordinates: Coordinates
-     * 
-     * Coordinates of the house 
-     */
-    export let coordinates: Coordinates;
-    /**
-     * param: gasStations: EESSPrecio[]
-     * 
-     * Array of gas stations to be displayed as markers
-     */
-    export let gasStations: EESSPrecio[];
+  /**
+   * param: coordinates: Coordinates
+   *
+   * Coordinates of the house
+   */
+  export let coordinates: Coordinates;
+  /**
+   * param: gasStations: EESSPrecio[]
+   *
+   * Array of gas stations to be displayed as markers
+   */
+  export let gasStations: EESSPrecio[];
 </script>
 
-<figure class="map" use:setMap/>
+<figure class="map" use:setMap />
+
+<style>
+  .map {
+    height: 40vh;
+  }
+</style>
--- A2clienteREST/src/pages/examples/README.md
+++
@@ -1,3 +0,0 @@
-Sample pages to get started with astro
-
-<!-- TODO Remove this folder on milestone end-->
